c4b990fb1f3462e8cdee4627f2fbc04f67b4741c
Christian <christian.rahn@outlook.de>
2020-10-26 11:27:15 +0100

    Add put method for contacts controller

--- index.js
+++ index.js
@@ -3,13 +3,45 @@ var app = express();
 var fs = require('fs');
 
 app.get('/contacts', function (req, res) {
-  fs.readFile(__dirname + '/data/contacts.json', 'utf8', function (err, data) {
+  fs.readFile(__dirname + '/data/contacts.json', 'utf8', (err, data) => {
     console.log(data);
     res.end(data);
   });
 });
 
-var server = app.listen(8081, function() {
+app.put('/contacts', (req, res) => {
+  /**
+   * Read the contact scheme and check if the passed data match the scheme.
+   * Unexpected fields will be cut.
+   */
+  var newContact = {};
+  fs.readFile(__dirname + '/data/contact_scheme.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    for (var key in data) {
+      if (req.query[key]) {
+        newContact[key] = req.query[key];
+      }
+    }
+  });
+
+  /**
+   * Read the contact list and append the new contact.
+   */
+  fs.readFile(__dirname + '/data/contact_list.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    data.push(newContact);
+    console.log(data);
+    fs.writeFile(__dirname + '/data/contact_list.json', JSON.stringify(data), (err) => {
+      if (err) throw err;
+      console.log('saved');
+    });
+  });
+  res.end();
+});
+
+var server = app.listen(8081, () => {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Corona tracker is listening to at http://%s:%s', host, port);
